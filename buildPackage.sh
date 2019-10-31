47ed21fad1cd646feb0275d03863a855ee10bc8b
Nickolay Grebenshikov <grebenshikov.n@gmail.com>
2019-10-31 14:12:51 +0700

    rebuild iphonendlls with -O0

--- buildPackage.sh
+++ buildPackage.sh
@@ -4,5 +4,5 @@ cd "$dir"
 rm -rf project/obj
 lime rebuild . ios || exit
 rm -rf project/obj
-rm -f extension-admob.zip
-zip -r extension-admob.zip extension haxelib.json include.xml project ndll dependencies frameworks
+#rm -f extension-admob.zip
+#zip -r extension-admob.zip extension haxelib.json include.xml project ndll dependencies frameworks
